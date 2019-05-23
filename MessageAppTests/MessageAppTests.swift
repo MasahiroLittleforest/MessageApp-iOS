888e731f308baf747ca1cbaa61f255718d500d51
github MasahiroLittleforest <masahiro.littleforest@gmail.com>
2019-05-23 12:07:14 +0900

    Initial Commit

---
+++ MessageAppTests/MessageAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  MessageAppTests.swift
+//  MessageAppTests
+//
+//  Created by 小森 将大 on 2019/05/23.
+//  Copyright © 2019 小森 将大. All rights reserved.
+//
+
+import XCTest
+@testable import MessageApp
+
+class MessageAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
